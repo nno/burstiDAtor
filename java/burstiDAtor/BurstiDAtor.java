38888cef0bb7e066ebfa28b77a6fe8e0183b9062
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2014-03-16 20:23:04 +0100

    SML: add newlines to license in java code, so that the info dialog box does not become too big

--- java/burstiDAtor/BurstiDAtor.java
+++ java/burstiDAtor/BurstiDAtor.java
@@ -16,22 +16,22 @@ public class BurstiDAtor extends JFrame implements ActionListener {
     final static String VERSION = "0.10", NAME = "BurstiDAtor",
             DESC = "a neural spike/burst detector";
     final static String LICENSE_BODY = "Permission is hereby granted, free "
-            + "of charge, to any person obtaining a copy of this software "
+            + "of charge, to any person obtaining a copy of this software\n"
             + "and associated documentation files (the \"Software\"), to "
-            + "deal in the Software without restriction, including without "
+            + "deal in the Software without restriction\n, including without "
             + "limitation the rights to use, copy, modify, merge, publish, "
-            + "distribute, sublicense, and/or sell copies of the Software, "
+            + "distribute, sublicense,\nand/or sell copies of the Software, "
             + "and to permit persons to whom the Software is furnished to "
-            + "do so, subject to the following conditions:\n\nThe above "
+            + "do so,\nsubject to the following conditions:\n\nThe above "
             + "copyright notice and this permission notice shall be included "
             + "in all copies or substantial portions of the Software.\n\nTHE "
             + "SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, "
-            + "EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE "
-            + "WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR "
+            + "EXPRESS\n OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE "
+            + "WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR\n"
             + "PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR "
-            + "COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER "
+            + "COPYRIGHT HOLDERS BE LIABLE FOR\n ANY CLAIM, DAMAGES OR OTHER "
             + "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR "
-            + "OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE "
+            + "OTHERWISE,\nARISING FROM, OUT OF OR IN CONNECTION WITH THE "
             + "SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
     final static String LICENSE = String.format("Copyright %s\n\n%s", AUTHORS,
             LICENSE_BODY);
