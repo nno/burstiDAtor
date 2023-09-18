2323c4451b3b0756e378a5add83e54b909660597
ramroomh <rhamati95@gmail.com>
2023-09-18 11:48:06 -0400

    mPFC glutamatergic burst analysis 45ms parameter

--- java/burstiDAtor/Settings.java
+++ java/burstiDAtor/Settings.java
@@ -19,9 +19,9 @@ public class Settings extends Props {
         put("RTN_maxburstcontinue", .01d);
         put("RTN_minspikesinburst", 2);
 
-        put("GLU_maxburststart", .016d);
-        put("GLU_maxburstcontinue", .016d);
-        put("GLU_minspikesinburst", 2);
+        put("PFC_maxburststart", .045d);
+        put("PFC_maxburstcontinue", .045d);
+        put("PFC_minspikesinburst", 2);
 
         put("autocorr_min", "0.00000");
         put("autocorr_max", "2.00000");
@@ -33,7 +33,7 @@ public class Settings extends Props {
         put("supported_outputs","graphs,no graphs");
         put("output", getS("supported_outputs").split(",")[0]);
 
-        put("supported_neuron_types","DA,5HT,RTN,GLU");
+        put("supported_neuron_types","DA,5HT,RTN,PFC");
         put("neuron_type",getS("supported_neuron_types").split(",")[0]);
 
         put("fieldsep", "\t");
